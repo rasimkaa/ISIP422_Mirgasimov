5fdb66e962f4eb1810558fbf44a81c4fa6591df0
227134 <227134@KB37-303-C3>
2025-09-22 13:35:29 +0300

    Добавлен класс товаров и мтеоды для продаи и просчитывания остатков

---
+++ prakt1/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt1
+{
+    public class Product
+    {
+        private static int counter = 1;
+
+        public int Code { get; private set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public bool InStock => Quantity > 0;
+        public ProductCategory Category { get; set; }
+
+        public Product(string name, decimal price, int quantity, ProductCategory category)
+        {
+            Code = counter++;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Category = category;
+        }
+
+        public void Sell(int amount)
+        {
+            if (Quantity >= amount)
+                Quantity -= amount;
+            else
+                throw new InvalidOperationException("Недостаточно товара на складе!");
+        }
+
+        public void Restock(int amount)
+        {
+            Quantity += amount;
+        }
+    }
