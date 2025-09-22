c8ea8985a341514cdd222cea8bcd9af156142b4b
227134 <227134@KB37-303-C3>
2025-09-22 13:48:13 +0300

    Создан класс продаж для доп. задания

---
+++ prakt1/Sale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt1
+{
+    public class Sale
+    {
+        public Product Product { get; }
+        public int Quantity { get; }
+        public decimal Total => Product.Price * Quantity;
+
+        public Sale(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+    }
+}
